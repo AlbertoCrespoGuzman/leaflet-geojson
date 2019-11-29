d5b610213dc50ae24fca5b57c2179cbfb2397996
Alberto Crespo <alberto@qualityanalytics.io>
2019-11-29 19:41:21 -0300

    working sample

---
+++ data/converted-data1.js
@@ -0,0 +1,359 @@
+var array = [{"id": "7",
+"incidentid": "B3FEC9135164584EC6475619DFE0E2A6",
+"createddated": "2015-12-31 23:36:00 EST",
+"incidentcategory": "Fire",
+"incidenttype": "2 Alarm Fire",
+"street": "30 Bergen Ave",
+"city": "Jersey City",
+"stateprovince": "NJ",
+"postal": "07305",
+"country": "United States",
+"latitude": 40.70452,
+"longitude": -74.08889,
+"activitystatus": "OPEN",
+"severity": "Media",
+"gist": "Bergen Ave & Woodlawn Ave- There are reports of a 2 alarm fire in a residence. No injuries.",
+"description": "There are reports of a 2 alarm fire in a residence on Bergen Ave and Woodlawn Ave. No injuries have been reported at this time.",
+"notifyrule": "Use Perimeter",
+"infoquality": "Unconfirmed",
+"infosource": "Unconfirmed",
+"year": "2015",
+"id_numner": 7},
+{"id": "822",
+"incidentid": "19E8D42BBFEE9113C6F8328C2663DE3D",
+"createddated": "2015-12-30 10:07:00 EST",
+"incidentcategory": "Fire",
+"incidenttype": "1 Alarm Fire",
+"street": "160 Brunswick St",
+"city": "Jersey City",
+"stateprovince": "NJ",
+"postal": "07302",
+"country": "United States",
+"latitude": 40.72478,
+"longitude": -74.07053,
+"activitystatus": "CLOSED",
+"severity": "Minor",
+"gist": "Brunswick St & 5th St - No injuries were reported in the one-alarm fire. Incident closed.",
+"description": "Unconfirmed reports indicated that crews responded to the scene of a one-alarm fire on Brunswick St, near 5th St. There were no reports of injuries. This incident is closed.",
+"notifyrule": "Use Perimeter",
+"infoquality": "Unconfirmed",
+"infosource": "Unconfirmed",
+"year": "2015",
+"id_numner": 822},
+{"id": "864",
+"incidentid": "19E8D42BBFEE9113C6F8328C2663DE3D",
+"createddated": "2015-12-30 10:07:00 EST",
+"incidentcategory": "Fire",
+"incidenttype": "1 Alarm Fire",
+"street": "160 Brunswick St",
+"city": "Jersey City",
+"stateprovince": "NJ",
+"postal": "07302",
+"country": "United States",
+"latitude": 40.72478,
+"longitude": -74.05053,
+"activitystatus": "OPEN",
+"severity": "Minor",
+"gist": "Brunswick St & 5th St - Crews are reportedly responding to the scene of a one-alarm fire in the area",
+"description": "Unconfirmed reports indicate that crews are responding to the scene of a one-alarm fire on Brunswick St, near 5th St. There are no immediate reports of injuries.",
+"notifyrule": "Use Perimeter",
+"infoquality": "Unconfirmed",
+"infosource": "Unconfirmed",
+"year": "2015",
+"id_numner": 864},
+{"id": "5488",
+"incidentid": "2FEDEDE333363304D007EC7C28C53CB1",
+"createddated": "2015-12-20 08:05:00 EST",
+"incidentcategory": "Infrastructure",
+"incidenttype": "Water Main Break",
+"street": "173 Sterling Ave",
+"city": "Jersey City",
+"stateprovince": "NJ",
+"postal": "07305",
+"country": "United States",
+"latitude": 40.70762,
+"longitude": -74.07189,
+"activitystatus": "CLOSED",
+"severity": "Minor",
+"gist": "Sterling Ave & Fulton Ave - No major disruptions reported from water main break. Incident closed.",
+"description": "There were reports of a water main break on Sterling Ave, near Fulton Ave. There were no reports of major disruptions. This incident is closed.",
+"notifyrule": "Use Perimeter",
+"infoquality": "Unconfirmed",
+"infosource": "Unconfirmed",
+"year": "2015",
+"id_numner": 5488},
+{"id": "5529",
+"incidentid": "2FEDEDE333363304D007EC7C28C53CB1",
+"createddated": "2015-12-20 08:05:00 EST",
+"incidentcategory": "Infrastructure",
+"incidenttype": "Water Main Break",
+"street": "173 Sterling Ave",
+"city": "Jersey City",
+"stateprovince": "NJ",
+"postal": "07305",
+"country": "United States",
+"latitude": 40.70762,
+"longitude": -74.09189,
+"activitystatus": "OPEN",
+"severity": "Minor",
+"gist": "Sterling Ave & Fulton Ave - There are reports of a water main break in the area.",
+"description": "There are reports of a water main break on Sterling Ave, near Fulton Ave. Expect disruptions in the area as crews respond to the break.",
+"notifyrule": "Use Perimeter",
+"infoquality": "Unconfirmed",
+"infosource": "Unconfirmed",
+"year": "2015",
+"id_numner": 5529},
+{"id": "5676",
+"incidentid": "692FE598A61C22E8A6D37CA5CE582C1F",
+"createddated": "2015-12-19 20:18:00 EST",
+"incidentcategory": "Security",
+"incidenttype": "Violent Crime",
+"street": "905 Bergen Ave",
+"city": "Jersey City",
+"stateprovince": "NJ",
+"postal": "07306",
+"country": "United States",
+"latitude": 40.72017,
+"longitude": -74.06532,
+"activitystatus": "CLOSED",
+"severity": "Minor",
+"gist": "Bergen Ave & Newkirk St - Police investigated a fatal shooting in the area. Incident closed.",
+"description": "Authorities investigated a shooting on Bergen Ave, near Newkirk St. One victim was reportedly found dead at the scene. Expect residual police activity in the area as the investigation remains ongoing. This incident is closed.",
+"notifyrule": "Use Perimeter",
+"infoquality": "Media",
+"infosource": "Media",
+"year": "2015",
+"id_numner": 5676},
+{"id": "5746",
+"incidentid": "692FE598A61C22E8A6D37CA5CE582C1F",
+"createddated": "2015-12-19 20:18:00 EST",
+"incidentcategory": "Security",
+"incidenttype": "Violent Crime",
+"street": "905 Bergen Ave",
+"city": "Jersey City",
+"stateprovince": "NJ",
+"postal": "07306",
+"country": "United States",
+"latitude": 40.73017,
+"longitude": -74.06532,
+"activitystatus": "OPEN",
+"severity": "Minor",
+"gist": "Bergen Ave & Newkirk St - Police are investigating a fatal shooting in the area.",
+"description": "Authorities are investigating a shooting on Bergen Ave, near Newkirk St. One victim was reportedly found dead at the scene. Expect continued police activity in the area as the investigation remains ongoing.",
+"notifyrule": "Use Perimeter",
+"infoquality": "Media",
+"infosource": "Media",
+"year": "2015",
+"id_numner": 5746},
+{"id": "5783",
+"incidentid": "D03AD63B7CED178B2E1EE216E5BD972C",
+"createddated": "2015-12-19 16:10:00 EST",
+"incidentcategory": "Fire",
+"incidenttype": "2 Alarm Fire",
+"street": "346 Communipaw Ave",
+"city": "Jersey City",
+"stateprovince": "NJ",
+"postal": "07304",
+"country": "United States",
+"latitude": 40.74117,
+"longitude": -74.06223,
+"activitystatus": "CLOSED",
+"severity": "Extreme",
+"gist": "Communipaw Ave & Whiton St - Crews contained the fire in a mixed-occupancy building. Incident closed",
+"description": "Crews contained a two-alarm fire in a mixed-occupancy building on Communipaw Ave, near Whiton St. There were no initial reports of injuries. This incident is closed.",
+"notifyrule": "Use Perimeter",
+"infoquality": "Media",
+"infosource": "Media",
+"year": "2015",
+"id_numner": 5783},
+{"id": "5798",
+"incidentid": "D03AD63B7CED178B2E1EE216E5BD972C",
+"createddated": "2015-12-19 16:10:00 EST",
+"incidentcategory": "Fire",
+"incidenttype": "2 Alarm Fire",
+"street": "346 Communipaw Ave",
+"city": "Jersey City",
+"stateprovince": "NJ",
+"postal": "07304",
+"country": "United States",
+"latitude": 40.71117,
+"longitude": -74.06223,
+"activitystatus": "OPEN",
+"severity": "Severe",
+"gist": "Communipaw Ave & Whiton St - A two-alarm fire has been reported in a residence.",
+"description": "A two-alarm fire has been reported in a residence on Communipaw Ave, near Whiton St. There are no initial reports of injuries.",
+"notifyrule": "Use Perimeter",
+"infoquality": "Unconfirmed",
+"infosource": "Unconfirmed",
+"year": "2015",
+"id_numner": 5798},
+{"id": "10201",
+"incidentid": "1331E5018852D59CDF7E4131E97C9747",
+"createddated": "2015-12-10 07:25:00 EST",
+"incidentcategory": "Hazmat",
+"incidenttype": "Hazmat Response",
+"street": "293 Manhattan Ave",
+"city": "Jersey City",
+"stateprovince": "NJ",
+"postal": "07307",
+"country": "United States",
+"latitude": 40.74685,
+"longitude": -74.06166,
+"activitystatus": "CLOSED",
+"severity": "Moderate",
+"gist": "Manhattan Ave & Tonnelle Ave - No reported injuries from hazmat spill. Incident closed.",
+"description": "Unconfirmed reports indicated that crews staged a hazmat response on Manhattan Ave, near Tonnelle Ave, for a spill of an unknown liquid. No injuries or major disruptions were reported. This incident is closed.",
+"notifyrule": "Use Perimeter",
+"infoquality": "Unconfirmed",
+"infosource": "Unconfirmed",
+"year": "2015",
+"id_numner": 10201}]
+
+function getData(){
+    var points = {}
+    points["type"] = "FeatureCollection"
+    points["features"] = []
+    colors = ["#ecfc00","#ff9933","#ff0000","#000000"]
+    
+    for (var i = 0; i < array.length; i++){
+        var point = {}
+        point["geometry"] = {}
+        point["geometry"]["type"] =  "Point"
+        point["geometry"]["coordinates"] = [ array[i]["longitude"],  array[i]["latitude"] ]
+        console.log("id = " + point["geometry"]["coordinates"] )
+        point["type"] = "Feature"
+        point["properties"] = {}
+        
+        point['properties']["incidentcategory"] = array[i]["incidentcategory"]
+        point['properties']["adddress"] = array[i]["street"] + " " + array[i]["city"] + " " + array[i]["country"]
+    
+        point["properties"]["popupContent"] = "<strong>Incident Category: </strong>" + point['properties']["incidentcategory"] + "<br/><br/><strong>Severity: </strong>" + array[i]["severity"] + "<br/><br/><strong>Address: </strong> " +  point['properties']["adddress"] + "<br/><br/><strong>Description: </strong>" + array[i]["description"]
+    
+        switch (array[i]["severity"]){
+            case "Minor":
+                point['properties']["color"] = colors[0]
+                break
+            case "Media":
+                point['properties']["color"] = colors[1]
+                break
+            case "Moderate":
+                point['properties']["color"] = colors[2]
+                break
+            case "Extreme":
+                point['properties']["color"] = colors[3]
+                break
+            default:
+                point['properties']["color"] = colors[0]
+                break
+        }
+        points["features"].push(point)
+    }
+    
+
+    return points
+}
+
+
+var njPoints = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9998241,
+                    39.7471494
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 51
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9983545,
+                    39.7502833
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 52
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9963919,
+                    39.7444271
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 54
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9960754,
+                    39.7498956
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 55
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9933717,
+                    39.7477264
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 57
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9913392,
+                    39.7432392
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 58
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9788452,
+                    39.6933755
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 74
+        }
+    ]
+};
